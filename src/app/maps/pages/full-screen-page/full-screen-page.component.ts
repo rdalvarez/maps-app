27f1aa581513e9b9d49fe657abc78d75a02c0fa5
David <alv.ricardodavid@gmail.com>
2024-08-19 21:02:34 -0300

    Mostrar mapa en pantalla completa

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWQtOTQxIiwiYSI6ImNtMDAzODVudjAyangybW9ndHBzcnYyaGwifQ.Hkip9P0odp1wTZ_OdItYOg';
+
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css',
   host: {'collision-id': 'full-screen'},
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+
 
 }
