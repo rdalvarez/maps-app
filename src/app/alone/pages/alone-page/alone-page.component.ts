39ff6e11771ac66b5d0f6b842aa95ac47ffea129
David <alv.ricardodavid@gmail.com>
2024-09-01 18:03:41 -0300

    Standalone - Prueba de componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   standalone: true,
+  imports: [ CounterAloneComponent ],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
 })
